eb246daac03d716e8bc4e27c03d2e816b7a64274
absek01 <a_shrestha@mail.fhsu.edu>
2024-10-08 17:49:25 -0400

    imported required libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+# Author: Abhisek Shrestha
+# Class: Data Science 101
+# Project: Data Generation, Analysis, and Visualization with Python
+
+# Importing necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from faker import Faker
+import numpy as np
+
